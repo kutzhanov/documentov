3679490999222a679683348c1689c506bef2be81
Timur Kutzhanov <kutzhanov@gmail.com>
2019-09-29 17:49:56 +0600

    added check

--- dockerfile/services.sh
+++ dockerfile/services.sh
@@ -1,8 +1,45 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-service mysql start
-service nginx start
-service php7.2-fpm start
-update-rc.d mysql defaults
-update-rc.d nginx enable
-chkconfig php-fpm on
+# start nginx
+./start_nginx.sh -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start nginx: $status"
+  exit $status
+fi
+
+# start mysql
+./start_mysql.sh -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start mysql: $status"
+  exit $status
+fi
+
+# start php-fpm
+./start_php_fpm.sh -D
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start php-fpm: $status"
+  exit $status
+fi
+
+while sleep 60; do
+  ps aux |grep nginx |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  ps aux |grep mysql |grep -q -v grep
+  PROCESS_2_STATUS=$?
+  ps aux |grep php-fpm |grep -q -v grep
+  PROCESS_3_STATUS=$?
+  # If the greps above find anything, they exit with 0 status
+  # If they are not both 0, then something is wrong
+  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0]; then
+    echo "One of the processes has already exited."
+    exit 1
+  fi
+done
+
+#update-rc.d mysql defaults
+#update-rc.d nginx enable
+#chkconfig php-fpm on
+#update-rc.d php7.2-fpm enable
